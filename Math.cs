95f9f5053779150892051457dd241966eb6f9541
Mario Munoz <mario.zonum@gmail.com>
2021-04-24 21:20:21 -0400

    Create Math.cs

---
+++ Math.cs
@@ -0,0 +1,22 @@
+using System;
+
+
+namespace Math.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            long userNum;
+            int addTwentyFive = 5;
+            float divideBy = 12.5f;
+            bool greaterThan = userNum > 50;
+            
+
+            Console.WriteLine("Please enter a number: ");
+            userNum = Console.ReadLine();
+
+
+        }
+    }
+}
