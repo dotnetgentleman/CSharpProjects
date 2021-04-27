57c8b2f252050585f275255c9d939b3f68b951e5
Mario Munoz <mario.zonum@gmail.com>
2021-04-27 13:31:37 -0400

    Update PackageExpress.cs

--- PackageExpress.cs
+++ PackageExpress.cs
@@ -1,14 +1,14 @@
 using System;
 
 
-namespace BooleanLogic
+namespace myConsoleProject.cs
 {
     class Program
     {
         static void Main()
         {
             int weight, width, height, length, totalDimensions, totalPrice;
-            bool isHeavy, isLarge;
+            
 
             Console.WriteLine("Welcome to Package Express. Please follow the instruction below. ");
             Console.WriteLine("Please enter the package weight.");
@@ -20,10 +20,10 @@ namespace BooleanLogic
             }
 
             Console.WriteLine("Please enter the package width.");
-            width =  Convert.ToInt32(Console.ReadLine());
+            width = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Please enter the package height.");
-            height =  Convert.ToInt32(Console.ReadLine());
+            height = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Please enter the package length.");
             length = Convert.ToInt32(Console.ReadLine());
@@ -33,12 +33,15 @@ namespace BooleanLogic
             if (totalDimensions > 50)
             {
                 Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
             }
 
-            totalPrice = (width * height * length) * weight;
+            totalPrice = ((width * height * length) * weight) / 100;
 
             Console.WriteLine("Your estimated toal for shipping this package is: $" + totalPrice);
             Console.WriteLine("Thank you!");
+            Console.ReadLine();
+
         }
     }
 }
