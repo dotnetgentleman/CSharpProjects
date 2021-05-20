c7073744cec15af48e846b7dafdfce174786201d
Mario Munoz <mario.zonum@gmail.com>
2021-05-20 18:16:22 -0400

    Complete Do While loop

--- DoWhile/DoWhile/Program.cs
+++ DoWhile/DoWhile/Program.cs
@@ -10,6 +10,35 @@ namespace DoWhile
     {
         static void Main(string[] args)
         {
+            // Create a while statement with a boolean comparison 
+            bool start;
+
+            Console.WriteLine("Would you like to start the program? true or false");
+            start = Convert.ToBoolean(Console.ReadLine());
+
+            while (start == true)
+            {
+                Console.WriteLine("You have started the program. ");
+                Console.ReadLine();
+
+                int i = 0;
+
+                do
+                {
+                    i++;
+                    Console.WriteLine(i);
+
+                } while (i < 20);
+                Console.ReadLine();
+
+                start = false;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Program terminated.");
+            Console.ReadLine();
+
+
             
         }
     }
