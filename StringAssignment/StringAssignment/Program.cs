e2849b29e9e2cf494ebd43ca2c24147377f5cc38
Mario Munoz <mario.zonum@gmail.com>
2021-05-20 18:46:05 -0400

    complete assignment

--- StringAssignment/StringAssignment/Program.cs
+++ StringAssignment/StringAssignment/Program.cs
@@ -10,6 +10,27 @@ namespace StringAssignment
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("One plus" + " two" + " is equal to three. ");
+            Console.ReadLine();
+
+            string name;
+            Console.WriteLine("what is your name? ");
+            name = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine();
+
+            StringBuilder paragraph = new StringBuilder();
+
+            paragraph.Append("This is the first line.");
+            Console.WriteLine(paragraph);
+            Console.ReadLine();
+            paragraph.Append("\nThis is the second line. ");
+            Console.WriteLine(paragraph);
+            Console.ReadLine();
+            paragraph.Append("\nThis is the final line. ");
+            Console.WriteLine(paragraph);
+            Console.ReadLine();
         }
     }
 }
