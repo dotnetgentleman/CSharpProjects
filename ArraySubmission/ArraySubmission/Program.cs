254bbf74cde5cee2394bddfee4ab828f70d33abd
Mario Munoz <mario.zonum@gmail.com>
2021-05-20 19:11:33 -0400

    create arrays

---
+++ ArraySubmission/ArraySubmission/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArraySubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
